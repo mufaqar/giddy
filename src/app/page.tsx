fb1182917a142386476faeb38a6c570fae45b145
mufaqar <mufaqar@gmail.com>
2024-01-10 13:17:51 +0500

    merge code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import Image from 'next/image'
 import Navbar from './components/header/navbar'
 import Hero from './components/hero/hero'
+import Trendingposts from './components/posts/trendingposts'
+import FeaturedContent from './components/posts/featured-content'
+import VideoSection from './components/posts/video-sec'
+import Poll_Section from './components/poll/poll-sec'
 
 export default function Home() {
   return (
@@ -14,6 +18,18 @@ export default function Home() {
           bg="hero-gradient"
           tourVideo={true}
         />
+        <section className='py-16'>
+            <div className='container mx-auto px-4 grid md:grid-cols-4 grid-cols-1 gap-7 '>
+                <div className=''>
+                    <Trendingposts />
+                </div>
+                <div className='md:col-span-3'>
+                    <FeaturedContent />
+                </div>
+            </div>
+        </section>
+        <VideoSection />
+        <Poll_Section />
     </>
   )
 }

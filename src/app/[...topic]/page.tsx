375509189c71e87df68e270791cd94f771925f8e
syed zunair gillani <zunairgillani54@gmail.com>
2024-01-17 18:59:42 +0500

    update

--- src/app/[...topic]/page.tsx
+++ src/app/[...topic]/page.tsx
@@ -43,6 +43,7 @@ export default async function Topics(props: any) {
         topic,
         featureArticlesByTopics
     } = await getData(props.params.topic);
+        console.log("🚀 ~ Topics ~ topic:", topic)
 
 
     const videoPost = articles.filter((item: IArticle) => item?.videoURL?.length > 10)
